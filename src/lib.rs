907982de6f5f4046530f1bc425f3b5cb1c4e98ee
Lucas Liu <sqooiddev@gmail.com>
2022-07-10 23:52:33 +1000

    removed useless test

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,3 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
