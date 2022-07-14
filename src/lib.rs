bbd66408912992d555f092d5a122f3afb508e53c
Lucas Liu <sqooiddev@gmail.com>
2022-07-14 14:15:36 +1000

    created fn for make nvapi version macro

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,7 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+pub fn make_nvapi_version<T>(version: u32) -> NvU32 {
+    std::mem::size_of::<T>() as u32 | version << 16
+}
