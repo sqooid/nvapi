bc6a8ab4655e6b725ad7fd92f1a917594e1ce0af
Lucas Liu <sqooiddev@gmail.com>
2022-07-16 17:18:21 +1000

    restored missing lib link

---
+++ build.rs
@@ -0,0 +1,21 @@
+use std::path::PathBuf;
+
+extern crate bindgen;
+
+fn main() {
+    println!("cargo:rustc-link-search=./include/bin");
+    println!("cargo:rustc-link-lib=static=nvapi64");
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .derive_default(true)
+        .generate()
+        .expect("Generate bindings");
+
+    let out_path = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Write bindings");
+}
