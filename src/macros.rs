55b2fedbf49b0a8ed7dd9a9bd32eb340caf8ad07
Lucas Liu <sqooiddev@gmail.com>
2022-08-26 18:21:45 +1000

    made nvapi version macro const #1

--- src/macros.rs
+++ src/macros.rs
@@ -1,5 +1,5 @@
 use crate::NvU32;
 
-pub fn make_nvapi_version<T>(version: u32) -> NvU32 {
+pub const fn make_nvapi_version<T>(version: u32) -> NvU32 {
     std::mem::size_of::<T>() as u32 | version << 16
 }
